fc322a2f0d4194963adc06d01ab73cd763669e1f
Vaengir <vaengir@gmx.de>
2022-10-27 18:34:22 +0200

    inital lua folder commit

---
+++ nvim/lua/weiberle/plugins.lua
@@ -0,0 +1,53 @@
+local fn = vim.fn
+
+-- Automatically install packer
+local install_path = fn.stdpath("data") .. "/site/pack/packer/start/packer.nvim"
+if fn.empty(fn.glob(install_path)) > 0 then
+  PACKER_BOOTSTRAP = fn.system({
+    "git",
+    "clone",
+    "--depth",
+    "1",
+    "https://github.com/wbthomason/packer.nvim",
+    install_path,
+  })
+  print("Installing packer close and reopen Neovim...")
+  vim.cmd([[packadd packer.nvim]])
+end
+
+-- Autocommand that reloads neovim whenever you save the plugins.lua file
+vim.cmd([[
+  augroup packer_user_config
+  autocmd!
+  autocmd BufWritePost plugins.lua source <afile> | PackerSync
+  augroup end
+]])
+
+-- Use a protected call so we don't error out on first use
+local status_ok, packer = pcall(require, "packer")
+if not status_ok then
+  return
+end
+
+-- Have packer use a popup window
+packer.init({
+  display = {
+    open_fn = function()
+      return require("packer.util").float({ border = "rounded" })
+    end,
+  },
+})
+
+-- Install your plugins here
+return packer.startup(function(use)
+  -- My plugins here
+  
+  use({ "wbthomason/packer.nvim" }) -- packer manage itself
+  use({ "windwp/nvim-autopairs", commit = "fa6876f832ea1b71801c4e481d8feca9a36215ec" }) -- Autopairs, integrates with both cmp and treesitter
+  use({ "numToStr/Comment.nvim" })
+  use({"jdhao/better-escape.vim", event = 'InsertEnter'})
+  use({"folke/which-key.nvim"})
+
+  -- Colorschemes
+  use "rmehri01/onenord.nvim"
+end)
