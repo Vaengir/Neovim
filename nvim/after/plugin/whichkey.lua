3ec8115acf4120e30db9dcc1203605c1e8e2cccf
Weiberle <maxweiberle63@gmail.com>
2022-12-23 19:55:11 +0100

    config overhaul

---
+++ nvim/after/plugin/whichkey.lua
@@ -0,0 +1,118 @@
+local status_ok, which_key = pcall(require, "which-key")
+if not status_ok then
+  return
+end
+
+local setup = {
+  plugins = {
+    marks = false, -- shows a list of your marks on ' and `
+    registers = true, -- shows your registers on " in NORMAL or <C-r> in INSERT mode
+    spelling = {
+      enabled = false, -- enabling this will show WhichKey when pressing z= to select spelling suggestions
+      suggestions = 20, -- how many suggestions should be shown in the list?
+    },
+    -- the presets plugin, adds help for a bunch of default keybindings in Neovim
+    -- No actual key bindings are created
+    presets = {
+      operators = false, -- adds help for operators like d, y, ... and registers them for motion / text object completion
+      motions = true, -- adds help for motions
+      text_objects = true, -- help for text objects triggered after entering an operator
+      windows = true, -- default bindings on <c-w>
+      nav = true, -- misc bindings to work with windows
+      z = true, -- bindings for folds, spelling and others prefixed with z
+      g = true, -- bindings for prefixed with g
+    },
+  },
+  -- add operators that will trigger motion and text object completion
+  -- to enable all native operators, set the preset / operators plugin above
+  -- operators = { gc = "Comments" },
+  key_labels = {
+    -- override the label used to display some keys. It doesn't effect WK in any other way.
+    -- For example:
+    -- ["<space>"] = "SPC",
+    -- ["<cr>"] = "RET",
+    -- ["<tab>"] = "TAB",
+  },
+  icons = {
+    breadcrumb = "»", -- symbol used in the command line area that shows your active key combo
+    separator = "➜", -- symbol used between a key and it's label
+    group = "+", -- symbol prepended to a group
+  },
+  popup_mappings = {
+    scroll_down = '<c-d>', -- binding to scroll down inside the popup
+    scroll_up = '<c-u>', -- binding to scroll up inside the popup
+  },
+  window = {
+    border = "none", -- none, single, double, shadow
+    position = "bottom", -- bottom, top
+    margin = { 1, 0, 1, 0 }, -- extra window margin [top, right, bottom, left]
+    padding = { 2, 2, 2, 2 }, -- extra window padding [top, right, bottom, left]
+    winblend = 0
+  },
+  layout = {
+    height = { min = 4, max = 25 }, -- min and max height of the columns
+    width = { min = 20, max = 50 }, -- min and max width of the columns
+    spacing = 3, -- spacing between columns
+    align = "left", -- align columns left, center or right
+  },
+  ignore_missing = true, -- enable this to hide mappings for which you didn't specify a label
+  hidden = { "<silent>", "<cmd>", "<Cmd>", "<CR>", "call", "lua", "^:", "^ " }, -- hide mapping boilerplate
+  show_help = true, -- show help message on the command line when the popup is visible
+  show_keys = true, -- show the currently pressed key and its label as a message in the command line
+  triggers = "auto", -- automatically setup triggers
+  -- triggers = {"<leader>"} -- or specify a list manually
+  triggers_blacklist = {
+    -- list of mode / prefixes that should never be hooked by WhichKey
+    -- this is mostly relevant for key maps that start with a native binding
+    -- most people should not need to change this
+    i = { "j", "k" },
+    v = { "j", "k" },
+  },
+  -- disable the WhichKey popup for certain buf types and file types.
+  -- Disabled by deafult for Telescope
+  disable = {
+    buftypes = {},
+    filetypes = { "TelescopePrompt" },
+  },
+}
+
+local opts = {
+  mode = "n", -- NORMAL mode
+  prefix = "<leader>",
+  buffer = nil, -- Global mappings. Specify a buffer number for buffer local mappings
+  silent = true, -- use `silent` when creating keymaps
+  noremap = true, -- use `noremap` when creating keymaps
+  nowait = true, -- use `nowait` when creating keymaps
+}
+
+local mappings = {
+  ["b"] = { "<cmd>lua require('telescope.builtin').buffers(require('telescope.themes').get_dropdown{previewer = false})<cr>", "Buffers" },
+  ["c"] = { "<cmd>bd<CR>", "Close Buffer" },
+  ["e"] = { "<cmd>NvimTreeToggle<cr>", "Explorer" },
+  ["g"] = { "<cmd>ZenMode | IndentBlanklineToggle<cr>", "ZenMode" },
+  ["o"] = { "<cmd>LspZeroFormat<cr>", "Format" },
+  ["u"] = { "<cmd>UndotreeToggle<CR>", "UndoTree" },
+  ["w"] = { "<cmd>w!<CR>", "Save" },
+  ["q"] = { "<cmd>q!<CR>", "Quit" },
+  f = {
+    name = "Fuzzy Find",
+    f = { "<cmd>Telescope find_files<cr>", "Find Files" },
+    g = { "<cmd>Telescope git_files<cr>", "Git Files" },
+    s = { "<cmd>Telescope live_grep theme=ivy<cr>", "Find Strings" },
+    q = { "<cmd>Telescope quickfix<cr>", "Quickfixlist" },
+    j = { "<cmd>Telescope jumplist<cr>", "Jumplist" },
+  },
+  h = {
+    name = "Harpoon",
+    h = { "<cmd>lua require('harpoon.ui').toggle_quick_menu()<cr>", "Quick Menu" },
+    m = { "<cmd>lua require('harpoon.mark').add_file()<cr>", "Mark File" },
+    d = { "<cmd>lua require('harpoon.mark').rm_file()<cr>", "Remove File" },
+    w = { "<cmd>lua require('harpoon.ui').nav_file(1)<cr>", "Go To Whichkey" },
+    c = { "<cmd>lua require('harpoon.ui').nav_file(2)<cr>", "Go To Plugins" },
+    -- o = { "<cmd>lua require('harpoon.ui').nav_file(3)<cr>", "Go To " },
+    -- p = { "<cmd>lua require('harpoon.ui').nav_file(4)<cr>", "Go To " },
+  },
+}
+
+which_key.setup(setup)
+which_key.register(mappings, opts)
---
+++ nvim/lua/weiberle/packer.lua
@@ -0,0 +1,149 @@
+local fn = vim.fn
+
+-- Automatically install packer
+local install_path = fn.stdpath("data") .. "/site/pack/packer/start/packer.nvim"
+if fn.empty(fn.glob(install_path)) > 0 then
+  PACKER_BOOTSTRAP = fn.system({
+    "git",
+    "clone",
+    "--depth",
+    "1",
+    "https://github.com/wbthomason/packer.nvim",
+    install_path,
+  })
+  print("Installing packer close and reopen Neovim...")
+  vim.cmd([[packadd packer.nvim]])
+end
+
+-- Autocommand that reloads neovim whenever you save the packer.lua file
+vim.cmd([[
+  augroup packer_user_config
+  autocmd!
+  autocmd BufWritePost packer.lua source <afile> | PackerSync
+  augroup end
+]])
+
+-- Use a protected call so we don't error out on first use
+local status_ok, packer = pcall(require, "packer")
+if not status_ok then
+  return
+end
+
+-- Have packer use a popup window
+packer.init({
+  display = {
+    open_fn = function()
+      return require("packer.util").float({ border = "rounded" })
+    end,
+  },
+})
+
+-- Install your plugins here
+return packer.startup(function(use)
+  -- My plugins here
+  -- Have Packer manage itself
+  use({ "wbthomason/packer.nvim" })
+
+  -- Telescope
+  use {
+    'nvim-telescope/telescope.nvim', tag = '0.1.0',
+    requires = { { 'nvim-lua/plenary.nvim' } }
+  }
+
+  -- Colorscheme
+  use({
+    'rmehri01/onenord.nvim',
+    as = 'onenord',
+    config = function()
+      vim.cmd('colorscheme onenord')
+    end
+  })
+
+  -- Treesitter
+  use({ 'nvim-treesitter/nvim-treesitter', run = ':TSUpdate' })
+  use('nvim-treesitter/playground')
+  use 'nvim-treesitter/nvim-treesitter-context'
+
+  -- Nvim-Tree
+  use {
+    'nvim-tree/nvim-tree.lua',
+    requires = {
+      'nvim-tree/nvim-web-devicons',
+    },
+  }
+
+  -- JK to exit Insert Mode
+  use({ "jdhao/better-escape.vim", event = "InsertEnter" })
+
+  -- LSP Setup
+  use {
+    'VonHeikemen/lsp-zero.nvim',
+    requires = {
+      -- LSP Support
+      { 'neovim/nvim-lspconfig' },
+      { 'williamboman/mason.nvim' },
+      { 'williamboman/mason-lspconfig.nvim' },
+
+      -- Autocompletion
+      { 'hrsh7th/nvim-cmp' },
+      { 'hrsh7th/cmp-buffer' },
+      { 'hrsh7th/cmp-path' },
+      { 'saadparwaiz1/cmp_luasnip' },
+      { 'hrsh7th/cmp-nvim-lsp' },
+      { 'hrsh7th/cmp-nvim-lua' },
+
+      -- Snippets
+      { 'L3MON4D3/LuaSnip' },
+      { 'rafamadriz/friendly-snippets' },
+    }
+  }
+
+  -- Startscreen for Vim
+  use({ "goolord/alpha-nvim" })
+
+  -- Custom Statusbar
+  use({ "nvim-lualine/lualine.nvim" })
+
+  -- Gitsigns
+  use({ "lewis6991/gitsigns.nvim" })
+
+  -- Show indented blanklines
+  use({ "lukas-reineke/indent-blankline.nvim" })
+
+  -- Undotree
+  use({ "mbbill/undotree" })
+
+  -- Zen mode
+  use { "folke/zen-mode.nvim" }
+
+  -- Harpoon
+  use { "theprimeagen/harpoon" }
+
+  -- Comments
+  use({
+    "numToStr/Comment.nvim",
+    config = function()
+      require('Comment').setup()
+    end
+  })
+
+  -- Autopairs
+  use {
+    "windwp/nvim-autopairs",
+    config = function() require("nvim-autopairs").setup {} end
+  }
+  -- Vim Fugitive
+  use({ "tpope/vim-fugitive" })
+
+  -- Whichkey
+  use('folke/which-key.nvim')
+
+  -- VimBeGood
+  use({ "ThePrimeagen/vim-be-good" })
+
+  -- Automatically set up your configuration after cloning packer.nvim
+  -- Put this at the end after all plugins
+  if PACKER_BOOTSTRAP then
+    require("packer").sync()
+  end
+end)
