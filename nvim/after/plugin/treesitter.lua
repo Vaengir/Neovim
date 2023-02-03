20255daeb61c366a8db3bc2c008805bc0dc26cb3
Vaengir <vaengir@gmx.de>
2023-02-03 18:10:02 +0100

    added vim to ensure installed

--- nvim/after/plugin/treesitter.lua
+++ nvim/after/plugin/treesitter.lua
@@ -4,7 +4,7 @@ if not status_ok then
 end
 
 configs.setup({
-	ensure_installed = { "java", "lua", "html", "css", "php", "bash", "latex", "python", "markdown" },
+	ensure_installed = { "java", "lua", "html", "css", "php", "bash", "latex", "python", "markdown", "vim" },
 	highlight = {
 		enable = true, -- false will disable the whole extension
 	},
