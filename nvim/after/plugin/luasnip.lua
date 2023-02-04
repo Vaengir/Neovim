785fae80822e5b9367a29ccf9f75d5d291ca4928
Weiberle17 <maxweiberle63@gmail.com>
2023-02-04 12:31:00 +0100

    added snippet for footcite

--- nvim/after/plugin/luasnip.lua
+++ nvim/after/plugin/luasnip.lua
@@ -27,38 +27,38 @@ local conds_expand = require("luasnip.extras.conditions.expand")
 
 -- Options
 ls.setup({
-	history = true,
-	-- Update more often, :h events for more info.
-	update_events = "TextChanged,TextChangedI",
-	-- Snippets aren't automatically removed if their text is deleted.
-	-- `delete_check_events` determines on which events (:h events) a check for
-	-- deleted snippets is performed.
-	-- This can be especially useful when `history` is enabled.
-	delete_check_events = "TextChanged",
-	ext_opts = {
-		[types.choiceNode] = {
-			active = {
-				virt_text = { { "choiceNode", "Comment" } },
-			},
-		},
-	},
-	-- treesitter-hl has 100, use something higher (default is 200).
-	ext_base_prio = 300,
-	-- minimal increase in priority.
-	ext_prio_increase = 1,
-	enable_autosnippets = true,
-	-- mapping for cutting selected text so it's usable as SELECT_DEDENT,
-	-- SELECT_RAW or TM_SELECTED_TEXT (mapped via xmap).
-	store_selection_keys = "<Tab>",
-	-- luasnip uses this function to get the currently active filetype. This
-	-- is the (rather uninteresting) default, but it's possible to use
-	-- eg. treesitter for getting the current filetype by setting ft_func to
-	-- require("luasnip.extras.filetype_functions").from_cursor (requires
-	-- `nvim-treesitter/nvim-treesitter`). This allows correctly resolving
-	-- the current filetype in eg. a markdown-code block or `vim.cmd()`.
-	ft_func = function()
-		return vim.split(vim.bo.filetype, ".", true)
-	end,
+  history = true,
+  -- Update more often, :h events for more info.
+  update_events = "TextChanged,TextChangedI",
+  -- Snippets aren't automatically removed if their text is deleted.
+  -- `delete_check_events` determines on which events (:h events) a check for
+  -- deleted snippets is performed.
+  -- This can be especially useful when `history` is enabled.
+  delete_check_events = "TextChanged",
+  ext_opts = {
+    [types.choiceNode] = {
+      active = {
+        virt_text = { { "choiceNode", "Comment" } },
+      },
+    },
+  },
+  -- treesitter-hl has 100, use something higher (default is 200).
+  ext_base_prio = 300,
+  -- minimal increase in priority.
+  ext_prio_increase = 1,
+  enable_autosnippets = true,
+  -- mapping for cutting selected text so it's usable as SELECT_DEDENT,
+  -- SELECT_RAW or TM_SELECTED_TEXT (mapped via xmap).
+  store_selection_keys = "<Tab>",
+  -- luasnip uses this function to get the currently active filetype. This
+  -- is the (rather uninteresting) default, but it's possible to use
+  -- eg. treesitter for getting the current filetype by setting ft_func to
+  -- require("luasnip.extras.filetype_functions").from_cursor (requires
+  -- `nvim-treesitter/nvim-treesitter`). This allows correctly resolving
+  -- the current filetype in eg. a markdown-code block or `vim.cmd()`.
+  ft_func = function()
+    return vim.split(vim.bo.filetype, ".", true)
+  end,
 })
 
 -- Snippets
@@ -68,40 +68,53 @@ ls.add_snippets("all", {
 
 -- For Tex files
 ls.add_snippets("tex", {
+
   s("fig", {
-    t({"\\begin{figure}[htb]", "\\centering", "\\includesgraphics["}),
-    c(1, { t{"width="}, t{"height="}, }),
+    t({ "\\begin{figure}[htb]", "\\centering", "\\includesgraphics[" }),
+    c(1, { t { "width=" }, t { "height=" }, }),
     i(2),
     t("]{"),
     i(3),
-    t({"}", "\\caption["}),
+    t({ "}", "\\caption[" }),
     i(4),
-    t({"]{"}),
+    t({ "]{" }),
     rep(4),
-    t({" \\footnotemark}", "\\label{abb:"}),
+    t({ " \\footnotemark}", "\\label{abb:" }),
     i(5),
-    t({"}", "\\end{figure}", "\\footnotetext{"}),
-    c(6, { t{"Enthalten in: "}, t{"Mit Änderungen entnommen aus: "}, }),
-    t({"\\cite{"}),
+    t({ "}", "\\end{figure}", "\\footnotetext{" }),
+    c(6, { t { "Enthalten in: " }, t { "Mit Änderungen entnommen aus: " }, }),
+    t({ "\\cite{" }),
     i(7),
-    t({"}}", "", ""}),
-  })
+    t({ "}}", "", "" }),
+  }),
+
+  s("fc", {
+    t({ "\\footcite[" }),
+    c(1, { t { "" }, t { "Vgl. " }, }),
+    t({ "][" }),
+    c(2, { t { "" }, t { "S. " }, }),
+    i(3),
+    t({ "]{" }),
+    i(4),
+    t { "}" },
+  }),
+
 })
 
 -- Keymaps
-vim.keymap.set({ "i", "s" }, "<c-k>", function ()
+vim.keymap.set({ "i", "s" }, "<c-k>", function()
   if ls.expand_or_jumpable() then
     ls.expand_or_jump()
   end
 end, { silent = true })
 
-vim.keymap.set({ "i", "s" }, "<c-j>", function ()
+vim.keymap.set({ "i", "s" }, "<c-j>", function()
   if ls.jumpable(-1) then
     ls.jump(-1)
   end
 end, { silent = true })
 
-vim.keymap.set("i", "<c-l>", function ()
+vim.keymap.set("i", "<c-l>", function()
   if ls.choice_active() then
     ls.change_choice(1)
   end
