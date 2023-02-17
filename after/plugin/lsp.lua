cb3d45eea4cb85700035de2791d8392710fb59e6
Vaengir <vaengir@gmx.de>
2023-02-17 11:05:01 +0100

    updated ensure installed list

--- after/plugin/lsp.lua
+++ after/plugin/lsp.lua
@@ -15,15 +15,17 @@ lsp.preset('recommended')
 
 lsp.ensure_installed({
   "bashls",
-  "cssls",
   "cssmodules_ls",
   "eslint",
   "html",
   "jdtls",
   "lua_ls",
   "marksman",
+  "prettier",
   "pyright",
-  "tailwindcss"
+  "svelte",
+  "texlab",
+  "tsserver",
 })
 
 -- Preferences
@@ -32,7 +34,7 @@ lsp.set_preferences({
     error = " ",
     warn = " ",
     hint = " ",
-    info = " "
+    info = " ",
   },
   set_lsp_keymaps = false,
 })
@@ -46,7 +48,7 @@ lsp.configure('lua_ls', {
         version = 'LuaJIT',
       },
       diagnostics = {
-        globals = { 'vim', 'beautiful', 'awesome', 'client' },
+        globals = { 'vim', 'beautiful', 'awesome', 'client', },
       },
       workspace = {
         -- Make the server aware of Neovim runtime files
@@ -62,14 +64,14 @@ lsp.configure('lua_ls', {
           indent_size = "2",
           max_line_length = "unset",
           trailing_table_separator = "always",
-        }
+        },
       },
       -- Do not send telemetry data containing a randomized but unique identifier
       telemetry = {
         enable = false,
       },
-    }
-  }
+    },
+  },
 })
 
 -- Java LSP Setup
@@ -77,10 +79,10 @@ lsp.configure('jdtls', {
   settings = {
     java = {
       format = {
-        settings = { url = "~/.config/nvim/lua/weiberle/jdtls/formatter.xml" }
-      }
-    }
-  }
+        settings = { url = "~/.config/nvim/lua/weiberle/jdtls/formatter.xml", },
+      },
+    },
+  },
 })
 
 --   פּ ﯟ   some other good icons
@@ -113,11 +115,11 @@ local kind_icons = {
 }
 
 local cmp = require('cmp')
-local cmp_select = { behavior = cmp.SelectBehavior.Select }
+local cmp_select = { behavior = cmp.SelectBehavior.Select, }
 local cmp_mappings = lsp.defaults.cmp_mappings({
   ['<C-p>'] = cmp.mapping.select_prev_item(cmp_select),
   ['<C-n>'] = cmp.mapping.select_next_item(cmp_select),
-  ['<C-y>'] = cmp.mapping.confirm({ select = true }),
+  ['<C-y>'] = cmp.mapping.confirm({ select = true, }),
   ["<C-Space>"] = cmp.mapping.complete(),
 })
 
@@ -129,35 +131,35 @@ cmp_mappings['<S-Tab>'] = nil
 lsp.setup_nvim_cmp({
   mapping = cmp_mappings,
   sources = {
-    { name = "nvim_lsp" },
-    { name = "luasnip" },
-    { name = "buffer" },
-    { name = "path" },
+    { name = "nvim_lsp", },
+    { name = "luasnip", },
+    { name = "buffer", },
+    { name = "path", },
   },
   formatting = {
-    fields = { "kind", "abbr", "menu" },
+    fields = { "kind", "abbr", "menu", },
     format = function(entry, vim_item)
       -- Kind icons
       vim_item.kind = string.format("%s", kind_icons[vim_item.kind])
       -- vim_item.kind = string.format('%s %s', kind_icons[vim_item.kind], vim_item.kind) -- This concatonates the icons with the name of the item kind
       vim_item.menu = ({
-        nvim_lsp = "[LSP]",
-        luasnip = "[Snippet]",
-        buffer = "[Buffer]",
-        path = "[Path]",
-      })[entry.source.name]
+            nvim_lsp = "[LSP]",
+            luasnip = "[Snippet]",
+            buffer = "[Buffer]",
+            path = "[Path]",
+          })[entry.source.name]
       return vim_item
     end,
   },
   window = {
     documentation = {
-      border = { "╭", "─", "╮", "│", "╯", "─", "╰", "│" },
+      border = { "╭", "─", "╮", "│", "╯", "─", "╰", "│", },
     },
   },
 })
 
 lsp.on_attach(function(client, bufnr)
-  local opts = { buffer = bufnr, remap = false }
+  local opts = { buffer = bufnr, remap = false, }
   vim.keymap.set('n', '<leader>rn', vim.lsp.buf.rename, opts)
   vim.keymap.set('n', 'ga', vim.lsp.buf.code_action, opts)
   vim.keymap.set('n', 'gn', vim.diagnostic.goto_next, opts)
