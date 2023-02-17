5429ee6992e5b85c63b7207e83b938e03d18e9d7
Vaengir <vaengir@gmx.de>
2023-02-17 11:12:14 +0100

    updated ensure installed list

--- after/plugin/treesitter.lua
+++ after/plugin/treesitter.lua
@@ -1,16 +1,31 @@
 local status_ok, configs = pcall(require, "nvim-treesitter.configs")
 if not status_ok then
-	return
+  return
 end
 
 configs.setup({
-	ensure_installed = { "java", "lua", "html", "css", "php", "bash", "latex", "python", "markdown", "vim" },
-	highlight = {
-		enable = true, -- false will disable the whole extension
-	},
-	autopairs = {
-		enable = true,
-	},
-	indent = { enable = true, disable = {} },
+  ensure_installed = {
+    "bash",
+    "bibtex",
+    "css",
+    "html",
+    "java",
+    "javascript",
+    "lua",
+    "latex",
+    "markdown",
+    "php",
+    "python",
+    "svelte",
+    "typescript",
+    "vim",
+  },
+  highlight = {
+    enable = true, -- false will disable the whole extension
+  },
+  autopairs = {
+    enable = true,
+  },
+  indent = { enable = true, disable = {}, },
   additional_vim_regex_highlighting = false,
 })
