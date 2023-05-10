f74c7dd366c4d239cd4d47d93057abfa128c71cb
Weiberle17 <maxweiberle63@gmail.com>
2023-05-10 07:41:07 +0200

    fix: Vimtex formatting disabled
    Also potentially fixing weird copy bug

--- lua/weiberle/packer.lua
+++ lua/weiberle/packer.lua
@@ -164,6 +164,7 @@ return packer.startup(function(use)
     config = function()
       vim.g.vimtex_quickfix_open_on_warning = 0
       vim.g.vimtex_mappings_enabled = false
+      vim.g.vimtex_format_enabled = false
     end,
   })
 
