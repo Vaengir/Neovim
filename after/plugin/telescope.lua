652953b56dfe4bf9a1a0db00c0d1620e158792d7
Vaengir <vaengir@gmx.de>
2023-07-18 15:51:55 +0200

    feat: Function for Telescope
    Allows same keybind to open git files in a repo and all files when not
    in a repo.

--- after/plugin/telescope.lua
+++ after/plugin/telescope.lua
@@ -23,3 +23,19 @@ require('telescope').setup {
     },
   },
 }
+
+local M = {}
+
+-- call via:
+-- :lua require"telescope-config".project_files()
+M.project_files = function()
+  local opts = {} -- define here if you want to define something
+  vim.fn.system('git rev-parse --is-inside-work-tree')
+  if vim.v.shell_error == 0 then
+    require "telescope.builtin".git_files(opts)
+  else
+    require "telescope.builtin".find_files(opts)
+  end
+end
+
+return M
