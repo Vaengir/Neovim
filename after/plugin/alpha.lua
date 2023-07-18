6079d2112cd8b61bc121cdcd6d95ab566074ed3c
Weiberle17 <maxweiberle63@gmail.com>
2023-07-18 15:51:55 +0200

    feat: Function for Telescope
    Allows same keybind to open git files in a repo and all files when not
    in a repo.

--- after/plugin/alpha.lua
+++ after/plugin/alpha.lua
@@ -24,7 +24,7 @@ dashboard.section.header.val = {
 -- }
 
 dashboard.section.buttons.val = {
-  dashboard.button("f", "󰈞  Find file", ":Telescope git_files <CR>"),
+  dashboard.button("f", "󰈞  Find file", "<cmd>lua require'telescope'.project_files()<cr>"),
   dashboard.button("e", "  New file", ":ene <BAR> startinsert <CR>"),
   dashboard.button("r", "󰄉  Recently used files", ":Telescope oldfiles <CR>"),
   dashboard.button("t", "󰊄  Find text", ":Telescope live_grep <CR>"),
