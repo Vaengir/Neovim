a4ba5edf8ff8cd7ead3bdb0b925d1a814bf29f88
Weiberle17 <maxweiberle63@gmail.com>
2023-07-24 14:36:28 +0200

    fix: Function snippet

--- after/plugin/snippets/rust.lua
+++ after/plugin/snippets/rust.lua
@@ -53,8 +53,8 @@ ls.add_snippets("rust", {
       {
         c(1, { t { "", }, t { "pub ", }, }),
         i(2, "Name"),
-        c(3, { t { "", }, i(1, "Parameter: Type"), }),
-        c(4, { t { "", }, sn(nil, { t(" ->"), i(1, "RType"), }), }),
+        c(3, { t { "", }, i(1), }),
+        c(4, { t { "", }, sn(nil, { t(" -> "), i(1), }), }),
         i(0, "Body of the function goes here..."),
       }
     )
