3b3e6d8a7344a1946f8ab513628205402d88184f
Weiberle17 <maxweiberle63@gmail.com>
2023-10-09 11:23:10 +0200

    feat: Added InspectTree window

--- after/plugin/hardtime.lua
+++ after/plugin/hardtime.lua
@@ -54,7 +54,7 @@ hardtime.setup({
     ["<RIGHT>"] = { "", "i", },
   },
 
-  disabled_filetypes = { "qf", "netrw", "NvimTree", "lazy", "mason", "harpoon", "oil", "fugitive", "undotree", },
+  disabled_filetypes = { "qf", "netrw", "NvimTree", "lazy", "mason", "harpoon", "oil", "fugitive", "undotree", "query", },
 
   hints = {
     ["k%^"] = {
