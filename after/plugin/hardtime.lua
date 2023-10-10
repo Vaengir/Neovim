40a7558fddf131cc572f731edce8aabc9110329c
Vaengir <vaengir@gmx.de>
2023-10-10 09:13:56 +0200

    fix: Add help pages to ignored list

--- after/plugin/hardtime.lua
+++ after/plugin/hardtime.lua
@@ -54,7 +54,7 @@ hardtime.setup({
     ["<RIGHT>"] = { "", "i", },
   },
 
-  disabled_filetypes = { "qf", "netrw", "NvimTree", "lazy", "mason", "harpoon", "oil", "fugitive", "undotree", "query", },
+  disabled_filetypes = { "qf", "netrw", "NvimTree", "lazy", "mason", "harpoon", "oil", "fugitive", "undotree", "query", "help", },
 
   hints = {
     ["k%^"] = {
