70e6b190cb2c0ee042f5108fcd7c49b468d4a78d
Vaengir <vaengir@gmx.de>
2023-11-03 20:25:55 +0100

    feat: Autocmds for highlight on yank and removing trailing whitespaces

--- init.lua
+++ init.lua
@@ -1,1 +1,24 @@
 require("weiberle")
+
+local augroup = vim.api.nvim_create_augroup
+local WeiberleGroup = augroup('WeiberleGroup', {})
+
+local autocmd = vim.api.nvim_create_autocmd
+local yank_group = augroup('HighlightYank', {})
+
+autocmd('TextYankPost', {
+  group = yank_group,
+  pattern = '*',
+  callback = function()
+    vim.highlight.on_yank({
+      higroup = 'IncSearch',
+      timeout = 40,
+    })
+  end,
+})
+
+autocmd({ "BufWritePre", }, {
+  group = WeiberleGroup,
+  pattern = "*",
+  command = "%s/\\s\\+$//e",
+})
