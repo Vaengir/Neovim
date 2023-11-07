0383db707d4a96d56b69f44490075f2313a43785
Vaengir <vaengir@gmx.de>
2023-11-07 10:24:02 +0100

    fix: Revert back to packer
    Pckr doesn't seem to be working as well and the lockfile doesn't work
    which was the reason I switched in the first place.

--- lua/weiberle/packer.lua
+++ lua/weiberle/packer.lua
@@ -1,65 +1,88 @@
-local function bootstrap_pckr()
-  local pckr_path = vim.fn.stdpath("data") .. "/pckr/pckr.nvim"
+local fn = vim.fn
 
-  if not vim.loop.fs_stat(pckr_path) then
-    vim.fn.system({
-      'git',
-      'clone',
-      "--filter=blob:none",
-      'https://github.com/lewis6991/pckr.nvim',
-      pckr_path,
-    })
-  end
-
-  vim.opt.rtp:prepend(pckr_path)
+-- Automatically install packer
+local install_path = fn.stdpath("data") .. "/site/pack/packer/start/packer.nvim"
+if fn.empty(fn.glob(install_path)) > 0 then
+  PACKER_BOOTSTRAP = fn.system({
+    "git",
+    "clone",
+    "--depth",
+    "1",
+    "https://github.com/wbthomason/packer.nvim",
+    install_path,
+  })
+  print("Installing packer close and reopen Neovim...")
+  vim.cmd([[packadd packer.nvim]])
 end
 
-bootstrap_pckr()
+-- Autocommand that reloads neovim whenever you save the packer.lua file
+vim.cmd([[
+  augroup packer_user_config
+  autocmd!
+  autocmd BufWritePost packer.lua source <afile> | PackerSync
+  augroup end
+]])
+
+-- Use a protected call so we don't error out on first use
+local status_ok, packer = pcall(require, "packer")
+if not status_ok then
+  return
+end
+
+-- Have packer use a popup window
+packer.init({
+  display = {
+    open_fn = function()
+      return require("packer.util").float({ border = "rounded", })
+    end,
+  },
+})
 
 -- Install your plugins here
-require("pckr").add {
+return packer.startup(function(use)
+  -- My plugins here
+  -- Have Packer manage itself
+  use({ "wbthomason/packer.nvim", })
 
   -- Telescope
-  {
-    'nvim-telescope/telescope.nvim',
-    tag = '0.1.4',
+  use {
+    'nvim-telescope/telescope.nvim', tag = '0.1.4',
     requires = { { 'nvim-lua/plenary.nvim', }, },
-  },
+  }
 
   -- Colorscheme
-  {
+  use({
     'rmehri01/onenord.nvim',
     as = 'onenord',
     config = function()
       vim.cmd('colorscheme onenord')
     end,
-  },
+  })
 
   -- Treesitter
-  {
+  use({
     "nvim-treesitter/nvim-treesitter-textobjects",
     after = "nvim-treesitter",
     requires = "nvim-treesitter/nvim-treesitter",
-    run = ':TSUpdate',
-  },
-  { 'nvim-treesitter/nvim-treesitter-context', },
+  })
+  use 'nvim-treesitter/nvim-treesitter-context'
 
   -- Oil.nvim
-  {
+  use {
     'stevearc/oil.nvim',
     requires = { 'nvim-tree/nvim-web-devicons', },
-  },
+  }
 
   -- JK to exit Insert Mode
-  {
+  use {
     "max397574/better-escape.nvim",
     config = function()
       require("better_escape").setup()
     end,
-  },
+  }
 
   -- LSP Setup
-  {
+  use {
     'VonHeikemen/lsp-zero.nvim',
     branch = 'v2.x',
     requires = {
@@ -79,102 +102,106 @@ require("pckr").add {
       -- Snippets
       { 'L3MON4D3/LuaSnip', },
     },
-  },
+  }
 
   -- Startscreen for Vim
-  { "goolord/alpha-nvim", },
+  use({ "goolord/alpha-nvim", })
 
   -- Custom Statusbar
-  { "nvim-lualine/lualine.nvim", },
+  use({ "nvim-lualine/lualine.nvim", })
 
   -- Gitsigns
-  { "lewis6991/gitsigns.nvim", },
+  use({ "lewis6991/gitsigns.nvim", })
 
   -- Show indented blanklines
-  {
+  use({
     "lukas-reineke/indent-blankline.nvim",
     config = function()
       require("ibl").setup {
         scope = { enabled = false, },
       }
     end,
-  },
+  })
 
   -- Undotree
-  {
+  use({
     "mbbill/undotree",
     config = function()
       vim.g.undotree_WindowLayout = 2
       vim.g.undotree_SetFocusWhenToggle = 1
     end,
-  },
+  })
 
   -- Zen mode
-  { "folke/zen-mode.nvim", },
+  use { "folke/zen-mode.nvim", }
 
   -- Harpoon
-  { "theprimeagen/harpoon", },
+  use { "theprimeagen/harpoon", }
 
   -- Comments
-  {
+  use({
     "numToStr/Comment.nvim",
     config = function()
       require('Comment').setup()
     end,
-  },
+  })
 
   -- TODOs
-  {
+  use {
     "folke/todo-comments.nvim",
     requires = "nvim-lua/plenary.nvim",
     config = function()
       require("todo-comments").setup {
       }
     end,
-  },
+  }
 
   -- Autopairs
-  {
+  use {
     "windwp/nvim-autopairs",
     config = function() require("nvim-autopairs").setup {} end,
-  },
-
+  }
   -- Vim Fugitive
-  { "tpope/vim-fugitive", },
+  use({ "tpope/vim-fugitive", })
 
   -- LSP Status
-  { "j-hui/fidget.nvim",  tag = 'legacy', },
+  use({ "j-hui/fidget.nvim", tag = 'legacy', })
 
   -- Markdown Preview
-  {
+  use({
     "iamcco/markdown-preview.nvim",
     run = "cd app && npm install",
     setup = function() vim.g.mkdp_filetypes = { "markdown", } end,
     ft = { "markdown", },
-  },
+  })
 
   -- LaTeX Setup
-  {
+  use({
     "lervag/vimtex",
     config = function()
       vim.g.vimtex_quickfix_open_on_warning = 0
       vim.g.vimtex_mappings_enabled = false
       vim.g.vimtex_format_enabled = false
     end,
-  },
+  })
 
-  { "epwalsh/obsidian.nvim", },
+  use({ "epwalsh/obsidian.nvim", })
 
   -- Better Scrolloff
-  { 'Aasim-A/scrollEOF.nvim', },
+  use({ 'Aasim-A/scrollEOF.nvim', })
 
   -- Whichkey
-  { "folke/which-key.nvim", },
+  use({ "folke/which-key.nvim", })
 
   -- Hardtime
-  { "m4xshen/hardtime.nvim", },
+  use({ "m4xshen/hardtime.nvim", })
 
   -- VimBeGood
-  { "ThePrimeagen/vim-be-good", },
+  use({ "ThePrimeagen/vim-be-good", })
 
-}
+  -- Automatically set up your configuration after cloning packer.nvim
+  -- Put this at the end after all plugins
+  if PACKER_BOOTSTRAP then
+    require("packer").sync()
+  end
+end)
