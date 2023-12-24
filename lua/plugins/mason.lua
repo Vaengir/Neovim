5bb495966a0f50d9e1e30d8fd296193149938c54
Vaengir <vaengir@gmx.de>
2023-12-25 00:32:17 +0100

    style: Nicer way to set ensure_installed

--- lua/plugins/mason.lua
+++ lua/plugins/mason.lua
@@ -1,23 +1,19 @@
 return {
   "williamboman/mason.nvim",
   dependencies = { "williamboman/mason-lspconfig.nvim", },
-  config = function()
-    require("mason").setup()
-
-    require("mason-lspconfig").setup({
-      ensure_installed = {
-        "bashls",
-        "cssls",
-        "html",
-        "jdtls",
-        "kotlin_language_server",
-        "lua_ls",
-        "marksman",
-        "pyright",
-        "rust_analyzer",
-        "texlab",
-        "tsserver",
-      },
+  opts = function(_, opts)
+    vim.list_extend(opts.ensure_installed, {
+      "bashls",
+      "cssls",
+      "html",
+      "jdtls",
+      "kotlin_language_server",
+      "lua_ls",
+      "marksman",
+      "pyright",
+      "rust_analyzer",
+      "texlab",
+      "tsserver",
     })
   end,
 }
