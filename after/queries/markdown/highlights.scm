df43a8b20db53f2a8a253c7187158664cda77c81
Vaengir <vaengir@gmx.de>
2024-02-21 14:29:28 +0100

    feat: Set conceal chars with treesitter

---
+++ after/queries/markdown/highlights.scm
@@ -0,0 +1,15 @@
+;; extends
+
+(fenced_code_block
+  (fenced_code_block_delimiter) @markup.raw.delimiter
+  (#set! conceal "󰅴 "))
+
+(fenced_code_block
+  (info_string
+    (language) @conceal (#eq? @conceal "rust")
+    (#set! conceal " ")))
+
+(fenced_code_block
+  (info_string
+    (language) @conceal (#eq? @conceal "bash")
+    (#set! conceal "󱆃 ")))
