b1a6efbe696854d2d313812684aa426906c78ba7
Vaengir <vaengir@gmx.de>
2024-03-04 10:12:52 +0100

    feat: Table snippet

--- lua/snippets/tex.lua
+++ lua/snippets/tex.lua
@@ -50,6 +50,33 @@ rec_desc = function()
   )
 end
 
+local table_node
+table_node = function(args)
+  local tabs = {}
+  local count
+  table = args[1][1]:gsub("%s", ""):gsub("|", "")
+  count = table:len()
+  for j = 1, count do
+    local iNode
+    iNode = i(j)
+    tabs[2 * j - 1] = iNode
+    if j ~= count then
+      tabs[2 * j] = t " & "
+    end
+  end
+  return sn(nil, tabs)
+end
+
+local rec_table
+rec_table = function()
+  return sn(nil, {
+    c(1, {
+      t({ "", }),
+      sn(nil, { t { "\\\\", "", }, d(1, table_node, { ai[1], }), d(2, rec_table, { ai[1], }), }),
+    }),
+  });
+end
+
 return {
 
   s({ trig = "fig", name = "Figure", dscr = "Create figure template", },
@@ -452,5 +479,15 @@ return {
         i(1, "Bold text"),
       }
     )
+  ),
+
+  s({ trig = "table", name = "Table", dscr = "Create a dynamic table", }, {
+    t("\\begin{tabular}{"),
+    i(1, "0"),
+    t({ "}", "", }),
+    d(2, table_node, { 1, }, {}),
+    d(3, rec_table, { 1, }),
+    t({ "", "\\end{tabular}", }),
+  }
   ),
 }
