0a9b6ebea8634848901ffdab3965e32b98d50850
Vaengir <vaengir@gmx.de>
2024-03-20 10:34:37 +0100

    feat: Removed entirely
    See SHA: 157909efdf2ebca2d2ef2bdcf9db6729f6df4d67

--- lua/plugins/indent-blankline.lua
+++
@@ -1,9 +0,0 @@
-return {
-  "lukas-reineke/indent-blankline.nvim",
-  main = "ibl",
-  event = { "BufReadPre", "BufNewFile", },
-  version = "*",
-  -- opts = {
-  --   scope = { enabled = false, },
-  -- },
-}
