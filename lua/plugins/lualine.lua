204e672803667285a388301fe843eb7deaa17071
Vaengir <vaengir@gmx.de>
2024-03-20 10:32:11 +0100

    feat: Function to limit Strings based on window width

--- lua/plugins/lualine.lua
+++ lua/plugins/lualine.lua
@@ -8,6 +8,23 @@ return {
       return vim.fn.winwidth(0) > 80
     end
 
+    --- @param trunc_width number trunctates component when screen width is less then trunc_width
+    --- @param trunc_len number truncates component to trunc_len number of chars
+    --- @param hide_width number | nil hides component when window width is smaller then hide_width
+    --- @param no_ellipsis boolean whether to disable adding '...' at end after truncation
+    --- return function that can format the component accordingly
+    local function trunc(trunc_width, trunc_len, hide_width, no_ellipsis)
+      return function(str)
+        local win_width = vim.fn.winwidth(0)
+        if hide_width and win_width < hide_width then
+          return ""
+        elseif trunc_width and trunc_len and win_width < trunc_width and #str > trunc_len then
+          return str:sub(1, trunc_len) .. (no_ellipsis and "" or "...")
+        end
+        return str
+      end
+    end
+
     local diagnostics = {
       "diagnostics",
       sources = { "nvim_diagnostic", },
@@ -46,6 +63,7 @@ return {
       "branch",
       icons_enabled = true,
       icon = "",
+      fmt = trunc(120, 6, nil, true),
     }
 
     local cursor_position = {
@@ -65,8 +83,7 @@ return {
       options = {
         icons_enabled = true,
         theme = "onenord",
-        component_separators = { left = " ", right = "", },
-        -- component_separators = { left = "", right = "", },
+        component_separators = { left = "", right = "", },
         section_separators = { left = "", right = "", },
         disabled_filetypes = { "alpha", "dashboard", "NvimTree", "Outline", },
         always_divide_middle = true,
@@ -74,7 +91,7 @@ return {
       sections = {
         lualine_a = { branch, diagnostics, },
         lualine_b = { mode, },
-        lualine_c = { "filename", "%=", { "harpoon2", separator = " ", }, },
+        lualine_c = { { "filename", separator = "", }, { "%=", separator = "", }, { "harpoon2", separator = " ", }, },
         -- lualine_x = { "encoding", "fileformat", "filetype" },
         lualine_x = { --[[ {getGuiFont}, diff, ]] "encoding", filetype, },
         lualine_y = { cursor_position, },
