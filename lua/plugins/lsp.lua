852ad178277449ab533087a6031735974ae5f5cd
Vaengir <vaengir@gmx.de>
2024-04-01 20:19:02 +0200

    feat: Aditional settings for rust_analyser

--- lua/plugins/lsp.lua
+++ lua/plugins/lsp.lua
@@ -95,6 +95,27 @@ return {
     -- Rust LSP
     lspconfig.rust_analyzer.setup {
       capabilities = capabilities,
+      settings = {
+        ["rust-analyzer"] = {
+          imports = {
+            granularity = {
+              group = "module",
+            },
+            prefix = "self",
+          },
+          cargo = {
+            buildScripts = {
+              enable = true,
+            },
+          },
+          procMacro = {
+            enable = true,
+          },
+          checkOnSave = {
+            command = "clippy",
+          },
+        },
+      },
     }
 
     -- LaTeX LSP
