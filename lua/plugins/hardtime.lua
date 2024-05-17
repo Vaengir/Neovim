0ad3332bd7abb474702d65517a1b818b8d062597
Vaengir <vaengir@gmx.de>
2024-05-17 15:50:23 +0200

    feat: Only display hints, not block

--- lua/plugins/hardtime.lua
+++ lua/plugins/hardtime.lua
@@ -2,7 +2,21 @@ return {
   "m4xshen/hardtime.nvim",
   dependencies = { "MunifTanjim/nui.nvim", "nvim-lua/plenary.nvim", },
   opts = {
-    disabled_filetypes = { "qf", "netrw", "NvimTree", "lazy", "mason", "harpoon", "oil", "fugitive", "undotree", "query", "help", "packer", },
+    disabled_filetypes = {
+      "qf",
+      "netrw",
+      "NvimTree",
+      "lazy",
+      "mason",
+      "harpoon",
+      "oil",
+      "fugitive",
+      "undotree",
+      "query",
+      "help",
+      "packer",
+    },
     allow_different_key = true,
+    restriction_mode = "hint",
   },
 }
