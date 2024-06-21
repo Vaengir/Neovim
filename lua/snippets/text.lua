9dbf0756bbd122e69a2ee72051a7a0fe61ba21ca
Vaengir <vaengir@gmx.de>
2024-06-21 14:28:31 +0200

    fix: Copyright logo

--- lua/snippets/text.lua
+++ lua/snippets/text.lua
@@ -33,7 +33,7 @@ return {
       [[
         MIT License
 
-        Copyright (c) <> <>
+        Copyright © <> <>
 
         Permission is hereby granted, free of charge, to any person obtaining a copy
         of this software and associated documentation files (the "Software"), to deal
