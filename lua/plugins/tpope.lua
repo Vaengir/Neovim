16a49758d22eaa1e4df6eafe121dff51ada3ea3b
Vaengir <vaengir@gmx.de>
2025-02-11 12:56:53 +0100

    feat: Add user commands for fugitive-difftool

--- lua/plugins/tpope.lua
+++ lua/plugins/tpope.lua
@@ -10,7 +10,21 @@ return {
     "tpope/vim-fugitive",
     cmd = { "Git", "Ge", },
     dependencies = {
-      "jecaro/fugitive-difftool.nvim",
+      {
+        "jecaro/fugitive-difftool.nvim",
+        config = function()
+          -- Jump to the first quickfix entry
+          vim.api.nvim_create_user_command("Gcfir", require("fugitive-difftool").git_cfir, {})
+          -- To the last
+          vim.api.nvim_create_user_command("Gcla", require("fugitive-difftool").git_cla, {})
+          -- To the next
+          vim.api.nvim_create_user_command("Gcn", require("fugitive-difftool").git_cn, {})
+          -- To the previous
+          vim.api.nvim_create_user_command("Gcp", require("fugitive-difftool").git_cp, {})
+          -- To the currently selected
+          vim.api.nvim_create_user_command("Gcc", require("fugitive-difftool").git_cc, {})
+        end,
+      },
     },
     keys = {
       { "<leader>gd", "<cmd>Git pull --rebase<cr>",             desc = "Pull remote Changes", },
