06279b83e02327c50afc774f40b41e7ecaad7e06
Vaengir <vaengir@gmx.de>
2025-03-29 19:58:22 +0100

    feat: Display lsp diagnostics as virtual text.
    [Reddit comment](https://www.reddit.com/r/neovim/comments/1jm5atz/comment/mk9w6v0/?utm_source=share&utm_medium=web3x&utm_name=web3xcss&utm_term=1&utm_content=share_button)

--- lua/functions.lua
+++ lua/functions.lua
@@ -179,4 +179,28 @@ M.format_dat_sql = function(bufnr)
   end
 end
 
+---@param jumpCount number
+M.jumpWithVirtLineDiags = function(jumpCount)
+  pcall(vim.api.nvim_del_augroup_by_name, "jumpWithVirtLineDiags") -- prevent autocmd for repeated jumps
+
+  vim.diagnostic.jump { count = jumpCount, }
+
+  local initialVirtTextConf = vim.diagnostic.config().virtual_text
+  vim.diagnostic.config {
+    virtual_text = false,
+    virtual_lines = { current_line = true, },
+  }
+
+  vim.defer_fn(function() -- deferred to not trigger by jump itself
+    vim.api.nvim_create_autocmd("CursorMoved", {
+      desc = "User(once): Reset diagnostics virtual lines",
+      once = true,
+      group = vim.api.nvim_create_augroup("jumpWithVirtLineDiags", {}),
+      callback = function()
+        vim.diagnostic.config { virtual_lines = false, virtual_text = initialVirtTextConf, }
+      end,
+    })
+  end, 1)
+end
+
 return M
