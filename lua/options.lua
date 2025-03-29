2e66f2039f10aab6dee1701279326d84ff2cab04
Vaengir <vaengir@gmx.de>
2025-03-29 22:19:32 +0100

    feat: Added ability to use folds.

--- lua/options.lua
+++ lua/options.lua
@@ -55,3 +55,11 @@ vim.opt.listchars:append {
   nbsp = "○",
   eol = "↵",
 }
+
+vim.opt.foldenable = true
+vim.opt.foldlevel = 99
+vim.opt.foldmethod = "expr"
+vim.opt.foldexpr = "v:lua.vim.treesitter.foldexpr()"
+vim.opt.foldtext = ""
+vim.opt.foldcolumn = "0"
+vim.opt.fillchars:append({ fold = " ", })
