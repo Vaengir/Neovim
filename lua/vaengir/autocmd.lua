8cff0eed648c70e70e019fa488247cf7ffba4bd0
Vaengir <vaengir@gmx.de>
2025-06-02 14:44:18 +0200

    fix: Rename of vim.highlight to vim.hl

--- lua/vaengir/autocmd.lua
+++ lua/vaengir/autocmd.lua
@@ -6,7 +6,7 @@ autocmd("TextYankPost", {
   group = augroup("HighlightYank", {}),
   pattern = "*",
   callback = function()
-    vim.highlight.on_yank({
+    vim.hl.on_yank({
       higroup = "IncSearch",
       timeout = 100,
     })
