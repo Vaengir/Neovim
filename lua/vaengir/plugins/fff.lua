ae5711fc466ddcdfd0d60e7f5f0c5d3ba003021d
Vaengir <vaengir@gmx.de>
2025-09-20 12:51:28 +0200

    Plugin update

--- lua/vaengir/plugins/fff.lua
+++ lua/vaengir/plugins/fff.lua
@@ -1,6 +1,8 @@
 return {
   "dmtrKovalenko/fff.nvim",
-  build = "cargo build --release",
+  build = function()
+    require("fff.download").download_or_build_binary()
+  end,
   opts = {
     base_path = vim.fn.getcwd(),
     prompt = " ",
